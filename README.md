aa64547ace31a83fe20d782ba2c10d65fbf352a9
Aleksandr Iakovlev <aguinore@gmail.com>
2020-04-26 13:54:46 +0200

    used a library with stream support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # udemy_kafka_demo
 Implements Twitter client, Kafka Producer and consumer for https://www.udemy.com/course/apache-kafka/learn/
 
-Uses https://github.com/bear/python-twitter to connect to Twitter API 
+Uses https://github.com/tweepy/tweepy to connect to Twitter API 
--- kafka_demo_1/demo1.py
+++ kafka_demo_1/demo1.py
@@ -1,4 +1,4 @@
-import twitter
+from tweepy import StreamListener, OAuthHandler, Stream
 from yaml_config_parser import ConfigParser
 
 
@@ -12,8 +12,28 @@ class Secrets:
         self.access_token_secret = yaml.get_config('access_token_secret')
 
 
-secrets = Secrets()
-api = twitter.Api(consumer_key=secrets.consumer_key,
-                  consumer_secret=secrets.consumer_secret,
-                  access_token_key=secrets.access_token_key,
-                  access_token_secret=secrets.access_token_secret)
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+def create_twitter_client():
+    l = StdOutListener()
+    secrets = Secrets()
+    auth = OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+    auth.set_access_token(secrets.access_token_key, secrets.access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=['python', 'gvido'])
+
+
+client = create_twitter_client()
+#create a kafka producer
+#loop to send tweets to kafka
