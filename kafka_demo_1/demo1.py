b814bfa7f61f48fea68befd47000551c3836a8e5
Aleksandr Iakovlev <aguinore@gmail.com>
2020-05-01 11:12:28 +0200

    sending tweets to kafka

--- kafka_demo_1/demo1.py
+++ kafka_demo_1/demo1.py
@@ -1,5 +1,7 @@
 from tweepy import StreamListener, OAuthHandler, Stream
 from yaml_config_parser import ConfigParser
+import signal
+import sys
 
 
 class Secrets:
@@ -13,10 +15,15 @@ class Secrets:
 
 
 class StdOutListener(StreamListener):
+
+    def __init__(self, kafka_producer):
+        super().__init__()
+        self.kafka_producer = kafka_producer
+
     """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
     """
     def on_data(self, data):
+        self.kafka_producer.produce(topic='twitter_kafka', value=data)
         print(data)
         return True
 
@@ -24,16 +31,42 @@ class StdOutListener(StreamListener):
         print(status)
 
 
-def create_twitter_client():
-    l = StdOutListener()
+def exit_gracefully(kafka_producer):
+    if kafka_producer is not None:
+        kafka_producer.flush(30)
+        print('kafka producer flushed')
+    sys.exit(0)
+
+
+def create_twitter_client(kafka_producer):
+    listener = StdOutListener(kafka_producer)
     secrets = Secrets()
     auth = OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
     auth.set_access_token(secrets.access_token_key, secrets.access_token_secret)
 
-    stream = Stream(auth, l)
+    stream = Stream(auth, listener)
     stream.filter(track=['python', 'gvido'])
 
+    def signal_handler():
+        print('You pressed Ctrl+C!')
+        exit_gracefully(kafka_producer)
 
-client = create_twitter_client()
-#create a kafka producer
-#loop to send tweets to kafka
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGKILL, signal_handler)
+    return stream
+
+
+def create_kafka_producer():
+    # https://www.confluent.io/blog/introduction-to-apache-kafka-for-python-programmers/
+    from confluent_kafka import Producer
+
+    p = Producer({'bootstrap.servers': 'localhost:9092'})
+    return p
+
+
+producer = None
+try:
+    producer = create_kafka_producer()
+    create_twitter_client(producer)
+finally:
+    exit_gracefully(producer)
